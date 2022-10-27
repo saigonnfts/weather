7cd46a55e4763fda4aa660cdb8ba91b34f619b6f
Sai Gon NFTs <109427820+saigonnfts@users.noreply.github.com>
2022-10-27 21:11:05 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 //import {useState, useEffect} from 'react';
 //import axios from 'axios';
 
-//var App = require("./App.css");
+var App = require("./App.css");
 var useState = require("react");
 var useEffect = require("react");
 var axios = require("axios");
